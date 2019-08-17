007cbd0548db0cb70c4d6acd044cd014c037d4c3
derflacco <paul89rulez@gmail.com>
2019-08-17 15:15:03 +0200

    defconfig: enalbe cpusetassist and delete unused defconfigs

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -1781,11 +1781,10 @@ static ssize_t cpuset_write_resmask_wrapper(struct kernfs_open_file *of,
 	static struct cs_target cs_targets[] = {
 		/* Little-only cpusets go first */
 		{ "foreground",		"0-3" },
-		{ "background",		"0-1" },
+		{ "background",		"0-3" },
 		{ "system-background",	"0-3" },
 		{ "restricted",		"0-3" },
 		{ "top-app",		"0-7" },
-		{ "camera-daemon",	"0-7" },
 		};
 	struct cpuset *cs = css_cs(of_css(of));
 	int i;

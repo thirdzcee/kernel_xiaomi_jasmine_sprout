fa7a92c03912bddc14a76c4482fe13fa082fa092
ironbuang <thirdzcee@github.com>
2020-05-28 16:44:13 +0800

    always choose interactive when some postboot script decides to uae some other random governor
    Signed-off-by: ironbuang <thirdzcee@github.com>
    Signed-off-by: thirdzcee <thirdzcee@github.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -651,6 +651,10 @@ static int cpufreq_parse_governor(char *str_governor, unsigned int *policy,
 
 			if (ret == 0)
 				t = find_governor(str_governor);
+#ifdef CONFIG_CPU_FREQ_GOV_INTERACTIVE
+			else
+				t = find_governor("interactive");
+#endif
 		}
 
 		if (t != NULL) {
